2aafc4b1e0605313d81d95b08d7572a27110fe2e
Ryan Hansen <ryanhansen@rhansen-mac.lan>
2020-03-16 16:18:25 -0700

    toYYYYMMDDHH

--- @molonlabe/spartan-gql-date/src/utils.ts
+++ @molonlabe/spartan-gql-date/src/utils.ts
@@ -1,10 +1,17 @@
-export function toYYYYMMDD(date: Date, seperator = '') {
-  let month = '' + (date.getMonth() + 1);
-  let day = '' + date.getDate();
-  const year = date.getFullYear();
+export const toYYYYMMDD = (date: Date) => {
+  return Number(
+    date
+      .toISOString()
+      .replace(/[^0-9]+/g, '')
+      .substring(0, 8)
+  );
+};
 
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [year, month, day].join(seperator);
-}
+export const toYYYYMMDDHH = (date: Date) => {
+  return Number(
+    date
+      .toISOString()
+      .replace(/[^0-9]+/g, '')
+      .substring(0, 10)
+  );
+};
